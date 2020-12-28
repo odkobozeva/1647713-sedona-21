f11bc64b343a96ce8060917e6be764ad077cd079
odkobozeva <o.d.kobozeva@gmail.com>
2020-12-28 17:00:32 +0300

    Fix D7, D16

--- gulpfile.js
+++ gulpfile.js
@@ -95,7 +95,7 @@ exports.sprite = sprite;
 const copy = (done) => {
   gulp.src([
     "source/fonts/*.{woff2,woff}",
-    "source/*.ico",
+    "source/img/*.ico",
     "source/img/**/*.{jpg,png,svg}",
   ], {
     base: "source"
